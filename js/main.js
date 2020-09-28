f01c6a0c828dd597c0dff3229610b0e6e0fa561e
Zhanibek <zhann2013@gmail.com>
2020-09-28 23:20:47 +0600

    api request tested
    api request succesfully tested. audio tag working.

--- js/main.js
+++ js/main.js
@@ -1,20 +1,29 @@
 const url = "http://api.alquran.cloud/v1/quran/ar.alafasy";
 
-var xmlhttp = new XMLHttpRequest();
+var xhr = new XMLHttpRequest();
+xhr.onreadystatechange = function() {
+    if (xhr.readyState == XMLHttpRequest.DONE) {
+       let response = JSON.parse(this.responseText);
+       let element = "";
 
-xmlhttp.onreadystatechange = function() {
-  if (this.readyState == 4 && this.status == 200) {
-    var myArr = JSON.parse(this.responseText);
-    myFunction(myArr);
-  }
-};
-xmlhttp.open("GET", url, true);
-xmlhttp.send();
+      for (let i=0; i<1; i++) {
+        element +=  `<div class="surah_block surah-${i}"><h3 class="surah_title">${response.data.surahs[i].englishName}</h3>`;
 
-function myFunction(arr) {
-  var out = arr.edition;
-  
-  document.getElementById("test").innerHTML = out;
+        for (let j=0; j<response.data.surahs[i].ayahs.length; j++) {
+          element += `<p class="ayah_text">${response.data.surahs[i].ayahs[j].text}</p>`;
+          element += `<audio
+                          controls
+                          src="${response.data.surahs[i].ayahs[j].audio}">
+                              Your browser does not support the
+                              <code>audio</code> element.
+                      </audio>`;
+        }
+
+        element += `</div>` 
+      }
+
+       document.getElementById("surahs").innerHTML = element;
+    }
 }
-
-console.log("test");
+xhr.open('GET', url, true);
+xhr.send(null);
