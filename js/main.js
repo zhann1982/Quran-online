b30581c700df733dd3b19a45c8e7792dd97617bd
Zhanibek <zhann2013@gmail.com>
2020-09-29 07:29:20 +0600

    html and css basics
    Added some html and css. Slightly modified js

--- js/main.js
+++ js/main.js
@@ -1,29 +1,45 @@
 const url = "http://api.alquran.cloud/v1/quran/ar.alafasy";
+let curr_surah_num = 0;
+let response;
+
+let goto = function(num) {
+  let element = "";
+  element +=  `<div class="surah_block surah-${num}">
+                <h3 class="surah_title">${response.data.surahs[num].englishName}</h3>`;
+  for (let j = 0; j < response.data.surahs[num].ayahs.length; j++) {
+    element += `<p class="ayah_text">${response.data.surahs[num].ayahs[j].text}</p>`;
+    element += `<audio
+                    controls
+                    src="${response.data.surahs[num].ayahs[j].audio}">
+                        Your browser does not support the
+                        <code>audio</code> element.
+                </audio>`;
+  }
+  element += `</div>`;
+  document.getElementById("current_surah").innerHTML = element;
+}
 
 var xhr = new XMLHttpRequest();
 xhr.onreadystatechange = function() {
     if (xhr.readyState == XMLHttpRequest.DONE) {
-       let response = JSON.parse(this.responseText);
-       let element = "";
+      response = JSON.parse(this.responseText);
 
-      for (let i=0; i<1; i++) {
-        element +=  `<div class="surah_block surah-${i}"><h3 class="surah_title">${response.data.surahs[i].englishName}</h3>`;
+      // Create "current surah" block. Default is Al-Fatiha
+      goto(curr_surah_num);
 
-        for (let j=0; j<response.data.surahs[i].ayahs.length; j++) {
-          element += `<p class="ayah_text">${response.data.surahs[i].ayahs[j].text}</p>`;
-          element += `<audio
-                          controls
-                          src="${response.data.surahs[i].ayahs[j].audio}">
-                              Your browser does not support the
-                              <code>audio</code> element.
-                      </audio>`;
-        }
-
-        element += `</div>` 
+      // List of surahs for aside block
+      let list = `<ul class="list_surahs">`;
+      for (let i = 0; i < response.data.surahs.length; i++) {
+        list += `<li onclick="goto(${i})">Surah ${i+1}: ${response.data.surahs[i].englishName}</li>`;
       }
-
-       document.getElementById("surahs").innerHTML = element;
+      list += `</ul>`;
+      document.getElementById("list_of_surahs").innerHTML = list;
     }
+
+    // let link = document.getElementsByTagName("link");
+    // link.href = link.href + "?id=" + new Date().getMilliseconds();
+
+    
 }
 xhr.open('GET', url, true);
 xhr.send(null);
