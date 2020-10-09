4d05e61b1d1322a662c4b2630819e0b78b4e37c2
Zhanibek <zhann2013@gmail.com>
2020-10-09 06:50:44 +0600

    version 1 was deprecated
    I chose version 2. It is more fast at loading.

---
+++ js/deprecate.js
@@ -0,0 +1,57 @@
+const url = "http://api.alquran.cloud/v1/quran/ar.alafasy";
+
+let curr_surah_num = 0;
+let response;
+
+let goto = function(num) {
+  let element = "";
+  element +=  `<div class="surah_block surah-${num}">
+                <h2 class="surah_title">
+                  <img class="ornament" src="img/ornament.png">
+                    ${response.data.surahs[num].name}
+                  <img class="ornament" src="img/ornament.png">
+                </h2>
+                `;
+  for (let j = 0; j < response.data.surahs[num].ayahs.length; j++) {
+    element += `<p class="ayah_text">${response.data.surahs[num].ayahs[j].text} <span class="ayah_num">${j+1}</span></p>`;
+    element += `<audio class="audio_tag"
+                    controls
+                    src="${response.data.surahs[num].ayahs[j].audio}">
+                        Your browser does not support the
+                        <code>audio</code> element.
+                </audio>`;
+  }
+  element += `</div>`;
+  document.getElementById("current_surah").innerHTML = element;
+}
+
+var xhr = new XMLHttpRequest();
+xhr.onreadystatechange = function() {
+    if (xhr.readyState == XMLHttpRequest.DONE) {
+      response = JSON.parse(this.responseText);
+
+      // Create "current surah" block. Default is Al-Fatiha
+      goto(curr_surah_num);
+
+      // List of surahs for aside block
+      let list = `<h3>Surahs</h3><ul class="list_surahs">`;
+      for (let i = 0; i < response.data.surahs.length; i++) {
+        list += `<li class="${(i==curr_surah_num)?'active':''}" onclick="goto(${i})">${i+1} - ${response.data.surahs[i].englishName}</li>`;
+      }
+      list += `</ul>`;
+      document.getElementById("list_of_surahs").innerHTML = list;
+      document.getElementById("loader").style.display = "none";
+
+      let elementsArray = document.querySelectorAll("#list_of_surahs li");
+      elementsArray.forEach(function(elem) {
+          elem.addEventListener("click", function() {
+              elementsArray.forEach(function(el){
+                el.classList.remove("active");
+              });
+              elem.classList.add("active");
+          });
+      });
+    } 
+}
+xhr.open('GET', url, true);
+xhr.send(null);
--- js/main.js
+++ js/main.js
@@ -1,57 +1,77 @@
-const url = "http://api.alquran.cloud/v1/quran/ar.alafasy";
-
 let curr_surah_num = 0;
-let response;
+let response, response2;
 
-let goto = function(num) {
-  let element = "";
-  element +=  `<div class="surah_block surah-${num}">
-                <h2 class="surah_title">
-                  <img class="ornament" src="img/ornament.png">
-                    ${response.data.surahs[num].name}
-                  <img class="ornament" src="img/ornament.png">
-                </h2>
-                `;
-  for (let j = 0; j < response.data.surahs[num].ayahs.length; j++) {
-    element += `<p class="ayah_text">${response.data.surahs[num].ayahs[j].text} <span class="ayah_num">${j+1}</span></p>`;
-    element += `<audio class="audio_tag"
-                    controls
-                    src="${response.data.surahs[num].ayahs[j].audio}">
-                        Your browser does not support the
-                        <code>audio</code> element.
-                </audio>`;
-  }
-  element += `</div>`;
-  document.getElementById("current_surah").innerHTML = element;
+let goto = num => {
+    let url = `http://api.alquran.cloud/v1/surah/${num+1}/ar.alafasy`;
+
+    var xhr = new XMLHttpRequest();
+    xhr.onreadystatechange = function() {
+        if (xhr.readyState == XMLHttpRequest.DONE) {
+            response = JSON.parse(this.responseText);
+            
+            document.getElementById('surah_name').innerHTML = response.data.name;
+
+            let element = '';
+            for (let i=0; i<response.data.numberOfAyahs; i++) {
+                if (num==0 || num==8) {
+                    element += `<p class="ayah_text">${response.data.ayahs[i].text} <span class="ayah_num">${i+1}</span></p>`;
+                } else {
+                    if (i==0) {
+                        let ayah0 = (response.data.ayahs[i].text).replace('بِسْمِ ٱللَّهِ ٱلرَّحْمَٰنِ ٱلرَّحِيمِ ','');
+                        element += `<p class="ayah_text">بِسْمِ ٱللَّهِ ٱلرَّحْمَٰنِ ٱلرَّحِيمِ</p>`;
+                        element += `<audio class="audio_tag"
+                                        controls
+                                        src="./media/audio/bismillah.mp3">
+                                            Your browser does not support the
+                                            <code>audio</code> element.
+                                    </audio>`;
+                        element += `<p class="ayah_text">${ayah0} <span class="ayah_num">${i+1}</span></p>`;
+                    } else {
+                        element += `<p class="ayah_text">${response.data.ayahs[i].text} <span class="ayah_num">${i+1}</span></p>`;
+                    }
+                }
+                
+                element += `<audio class="audio_tag"
+                                controls
+                                src="${response.data.ayahs[i].audio}">
+                                    Your browser does not support the
+                                    <code>audio</code> element.
+                            </audio>`;
+            }
+            document.getElementById('surah_ayahs').innerHTML = element;
+        } 
+    }
+    xhr.open('GET', url, true);
+    xhr.send(null);
 }
 
-var xhr = new XMLHttpRequest();
-xhr.onreadystatechange = function() {
-    if (xhr.readyState == XMLHttpRequest.DONE) {
-      response = JSON.parse(this.responseText);
+goto(curr_surah_num);
 
-      // Create "current surah" block. Default is Al-Fatiha
-      goto(curr_surah_num);
+let url2 = `http://api.alquran.cloud/v1/meta`;
 
-      // List of surahs for aside block
-      let list = `<h3>Surahs</h3><ul class="list_surahs">`;
-      for (let i = 0; i < response.data.surahs.length; i++) {
-        list += `<li class="${(i==curr_surah_num)?'active':''}" onclick="goto(${i})">${i+1} - ${response.data.surahs[i].englishName}</li>`;
-      }
-      list += `</ul>`;
-      document.getElementById("list_of_surahs").innerHTML = list;
-      document.getElementById("loader").style.display = "none";
+var xhr2 = new XMLHttpRequest();
+xhr2.onreadystatechange = function() {
+    if (xhr2.readyState == XMLHttpRequest.DONE) {
+        response2 = JSON.parse(this.responseText);
 
-      let elementsArray = document.querySelectorAll("#list_of_surahs li");
-      elementsArray.forEach(function(elem) {
-          elem.addEventListener("click", function() {
-              elementsArray.forEach(function(el){
-                el.classList.remove("active");
-              });
-              elem.classList.add("active");
-          });
-      });
+        // List of surahs for aside block
+        let list = `<h3>Surahs</h3><ul class="list_surahs">`;
+        for (let i = 0; i < response2.data.surahs.count; i++) {
+            list += `<li class="${(i==curr_surah_num)?'active':''}" onclick="goto(${i})">${i+1} - ${response2.data.surahs.references[i].englishName}</li>`;
+        }
+        list += `</ul>`;
+        document.getElementById("list_of_surahs").innerHTML = list;
+
+        let elementsArray = document.querySelectorAll("#list_of_surahs li");
+        elementsArray.forEach(function(elem) {
+            elem.addEventListener("click", function() {
+                elementsArray.forEach(function(el){
+                    el.classList.remove("active");
+                });
+                elem.classList.add("active");
+            });
+        });
     } 
 }
-xhr.open('GET', url, true);
-xhr.send(null);
+xhr2.open('GET', url2, true);
+xhr2.send(null);
